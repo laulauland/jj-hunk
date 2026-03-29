e00a4a8dc8d2603d908cd00b75d4fdf63edd283a
Giovanni d'Amelio <gio@damelio.net>
2026-03-29 09:52:42 -0700

    fix(nix): read package version from Cargo.toml with git rev suffix

--- flake.nix
+++ flake.nix
@@ -3,43 +3,43 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "jj-hunk";
-            version = "0.1.0";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
-        });
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in let
+    cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    version = "${cargoToml.package.version}+${self.shortRev or self.dirtyShortRev or "unknown"}";
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "jj-hunk";
+        version = version;
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    });
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              cargo
-              clippy
-              rustc
-              rustfmt
-            ];
-          };
-        });
-    };
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          clippy
+          rustc
+          rustfmt
+        ];
+      };
+    });
+  };
 }
