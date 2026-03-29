7c5f8287262bbf399ba3e888f3d23a98602d3cc6
Giovanni d'Amelio <gio@damelio.net>
2026-03-29 11:44:06 -0700

    fix(nix): configure Nix test sandbox with jj-hunk in PATH and jj merge-tools config

--- flake.nix
+++ flake.nix
@@ -26,6 +26,17 @@
         version = version;
         src = ./.;
         cargoLock.lockFile = ./Cargo.lock;
+        nativeCheckInputs = with pkgs; [jujutsu git];
+        preCheck = ''
+          export HOME=$(mktemp -d)
+          mkdir -p $HOME/.config/jj
+          cat > $HOME/.config/jj/config.toml << 'TOML'
+          [merge-tools.jj-hunk]
+          program = "jj-hunk"
+          edit-args = ["select", "$left", "$right"]
+          TOML
+          export PATH="$PWD/target/${pkgs.stdenv.hostPlatform.rust.cargoShortTarget}/release:$PATH"
+        '';
       };
     });
 
